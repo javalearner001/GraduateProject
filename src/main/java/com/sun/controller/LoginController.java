fb299b8be52b5b5b856492468f3e3f5e223d6dfd
孙凯 <1403417950@qq.com>
2018-05-08 10:05:23 +0800

    logs

---
+++ src/main/java/com/sun/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.sun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @ClassName LoginController
+ * @Description TODO
+ * @Author sunkai
+ * @Date 2018/4/5 14:19
+ * @Version 1.0
+ **/
+@Controller
+public class LoginController {
+
+
+
+}
